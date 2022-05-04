1c9b896c7c634f4634e6b918df49c3188755c152
Cecilia Chan <cece@1x-nat-vl932-172-30-241-170.wireless.umass.edu>
2022-05-04 17:01:42 -0400

    Sub

--- server/index.js
+++ server/index.js
@@ -13,73 +13,95 @@ import { ScrabbleDatabase } from './wordDatabase.js';
 TODO: import the morgan middleware from 'morgan'
 TEMPLATE END */
 
-const WORD_SCORE_FILE = 'word-scores.json';
-const GAME_SCORE_FILE = 'game-scores.json';
 
-const database = new ScrabbleDatabase(process.env.DATABASE_URL);
 
-// Returns a function that will read a score file.
-function readScoreFile(path) {
-  return async () => {
-    try {
-      const scoreFile = await readFile(path, 'utf8');
-      const scores = JSON.parse(scoreFile);
-      return scores;
-    } catch (error) {
-      // Likely the file doesn't exist
-      return [];
-    }
-  };
+class ScrabbleServer {
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
+    this.app.use('/', express.static('client'));
+  }
+
+  async top10WordScores() {
+    const scores = await this.db.readAllWords(); // readWordScores();
+    const sorted = scores.sort((a, b) => b.score - a.score);
+    const top = sorted.slice(0, 10);
+    return top;
+  }
+  
+  async top10GameScores() {
+    const scores = await this.db.readAllGames(); // readGameScores();
+    //console.log(scores);
+    const sorted = scores.sort((a, b) => b.score - a.score);
+    const top = sorted.slice(0, 10);
+    return top;
+  }
+
+  async initRoutes() {
+    // Note: when using arrow functions, the "this" binding is lost.
+    const self = this;
+
+    this.app.post('/wordScore', async (request, response) => {
+      //await database.connect();
+      const { name, word, score } = request.body;
+      await this.db.createWord(name, word, score);
+      //await saveWordScore(name, word, score);
+      response.status(200).json({ status: 'success' });
+      //await database.close();
+    });
+
+    this.app.get('/highestWordScores', async (request, response) => {
+      const scores = await top10WordScores();
+      response.status(200).json(scores);
+    });
+
+    this.app.post('/gameScore', async (request, response) => {
+      //await database.connect();
+      const { name, score } = request.body;
+      await this.db.createGame(name, score);
+      //await saveGameScore(name, score);
+      response.status(200).json({ status: 'success' });
+      //await database.close();
+    });
+
+    this.app.get('/highestGameScores', async (request, response) => {
+      //await database.connect();
+      const scores = await top10GameScores();
+      response.status(200).json(scores);
+      //await database.close();
+    });
+
+    this.app.all('*', async (request, response) => {
+      response.status(404).send(`Not found: ${request.path}`);
+    });
+  }
+
+  async initDb() {
+    this.db = new ScrabbleDatabase(this.dburl);
+    await this.db.connect();
+  }
+
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 8080;
+    this.app.listen(port, () => {
+      console.log(`PeopleServer listening on port ${port}!`);
+    });
+  }
 }
 
-// Create functions for reading from score files.
-const readWordScores = readScoreFile(WORD_SCORE_FILE);
-const readGameScores = readScoreFile(GAME_SCORE_FILE);
-
-
-// Returns a function that will save a word score to a word score file.
-function saveToWordScoreFile(path) {
-  return async (name, word, score) => {
-    const data = { name, word, score };
-    const scores = await readWordScores();
-    scores.push(data);
-    writeFile(path, JSON.stringify(scores), 'utf8');
-  };
-}
-
-// Returns a function that will save a game score to a game score file.
-function saveToGameScoreFile(path) {
-  return async (name, score) => {
-    const data = { name, score };
-    const scores = await readGameScores();
-    scores.push(data);
-    writeFile(path, JSON.stringify(scores), 'utf8');
-  };
-}
-
-// Create functions for saving to score files.
-const saveWordScore = saveToWordScoreFile(WORD_SCORE_FILE);
-const saveGameScore = saveToGameScoreFile(GAME_SCORE_FILE);
+const server = new ScrabbleServer(process.env.DATABASE_URL);
+server.start();
 
 
 
-async function top10WordScores() {
-  const scores = await database.readAllWords(); // readWordScores();
-  const sorted = scores.sort((a, b) => b.score - a.score);
-  const top = sorted.slice(0, 10);
-  return top;
-}
 
-async function top10GameScores() {
-  const scores = await database.readAllGames(); // readGameScores();
-  //console.log(scores);
-  const sorted = scores.sort((a, b) => b.score - a.score);
-  const top = sorted.slice(0, 10);
-  return top;
-}
+
+
 
 // Create the Express app and set the port number.
-const app = express();
+/*const app = express();
 const port = process.env.PORT || 8080;
 
 // Add Middleware
@@ -89,43 +111,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use('/', express.static('client'));
 // SOLUTION END
-/** TEMPLATE BEGIN 
-TODO: Add the morgan middleware to the app.
-TODO: Add the express.static middleware to the app. 
- TEMPLATE END */
 
 
 // SOLUTION BEGIN
 app.post('/wordScore', async (request, response) => {
-  await database.connect();
+  //await database.connect();
   const { name, word, score } = request.body;
-  let value = await database.createWord(name, word, score);
+  await database.createWord(name, word, score);
   //await saveWordScore(name, word, score);
   response.status(200).json({ status: 'success' });
-  await database.close();
+  //await database.close();
 });
 
 app.get('/highestWordScores', async (request, response) => {
-  await database.connect();
   const scores = await top10WordScores();
   response.status(200).json(scores);
-  await database.close();
 });
 
 app.post('/gameScore', async (request, response) => {
-  await database.connect();
+  //await database.connect();
   const { name, score } = request.body;
-  let value = await database.createGame(name, score);
+  await database.createGame(name, score);
   //await saveGameScore(name, score);
   response.status(200).json({ status: 'success' });
-  await database.close();
+  //await database.close();
 });
 
 app.get('/highestGameScores', async (request, response) => {
-  await database.connect();
+  //await database.connect();
   const scores = await top10GameScores();
   response.status(200).json(scores);
-  await database.close();
+  //await database.close();
 });
 
 // This matches all routes that are not defined.
@@ -136,7 +152,7 @@ app.all('*', async (request, response) => {
 // Start the server.
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
-});
+}); */
 
 // SOLUTION END
 /** TEMPLATE BEGIN
